f3ba0877b2419faca3b44659cd3c951d101a2130
sjones97 <sjones97@email.unc.edu>
2019-10-11 17:24:54 -0400

    final commit

--- src/a5/GreenPlate.java
+++
@@ -1,9 +0,0 @@
-package a5;
-
-// Create Green Plate class
-public class GreenPlate extends PlateImpl{
-	
-	public GreenPlate(Sushi contents) throws PlatePriceException {
-		super(contents, Color.GREEN, 2.0);
-	}
-}
